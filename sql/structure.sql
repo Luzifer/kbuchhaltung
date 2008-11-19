6da6ec3ffd0156df6e155b05ef38a713bab368f8
Knut Ahlers <knut@ahlers.me>
2008-11-19 21:49:01 +0100

    Made the sql files processable with the new install script

--- sql/structure.sql
+++ sql/structure.sql
@@ -1,24 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 2.11.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Erstellungszeit: 12. August 2008 um 17:43
--- Server Version: 5.0.60
--- PHP-Version: 5.2.6-pl2-gentoo
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
---
--- Datenbank: `kbuchhaltung`
---
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `buchungen`
---
-
 CREATE TABLE IF NOT EXISTS `buchungen` (
   `id` bigint(20) NOT NULL auto_increment,
   `bez` varchar(255) NOT NULL,
@@ -27,13 +6,7 @@ CREATE TABLE IF NOT EXISTS `buchungen` (
   `gkonto` int(11) NOT NULL,
   `datum` date NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `konten`
---
+);
 
 CREATE TABLE IF NOT EXISTS `konten` (
   `id` int(11) NOT NULL auto_increment,
@@ -41,4 +14,4 @@ CREATE TABLE IF NOT EXISTS `konten` (
   `typ` enum('g','a','e') NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `bez` (`bez`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+);
